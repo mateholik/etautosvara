ce742df41f1535f0a8e10303bdfb3831bcb14b62
Vladis macbook <vladuxs1@gmail.com>
2025-03-09 23:22:39 +0200

    add site content

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+});
+
+// Close mobile menu when link is clicked
+document.querySelectorAll("#mobile-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("mobile-menu").classList.add("hidden");
+  });
+});
